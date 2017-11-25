3b46ae48a819c54232ce33b87b03bd9fe07ee72d
Pedro Wagner <pedrowagner505@gmail.com>
2017-11-25 14:14:44 -0200

    app e manual atualizados

--- IssuesAuthenticator/app/src/main/java/com/example/user/issuesauthenticator/DisplayMessageActivity.java
+++ IssuesAuthenticator/app/src/main/java/com/example/user/issuesauthenticator/DisplayMessageActivity.java
@@ -18,21 +18,30 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView txtlabid = (TextView) findViewById(R.id.labid);
         txtlabid.setText(labID);
 
-        Integer[] items = new Integer[]{16,17,18,19,20,21,22,23,24,25,26,27,28};
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, items);
-
-        Spinner mspin = (Spinner) findViewById(R.id.spinner3);
-        mspin.setAdapter(adapter);
-        mspin = (Spinner) findViewById(R.id.spinner4);
-        mspin.setAdapter(adapter);
+        Integer[] itemsTempMin = new Integer[]{16,17,18,19,20,21,22};
+        ArrayAdapter<Integer> adapterTempMin = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, itemsTempMin);
+        Integer[] itemsTempMax = new Integer[]{22,23,24,25,26,27,28};
+        ArrayAdapter<Integer> adapterTempMax = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, itemsTempMax);
+        Integer[] itemsUmiMin = new Integer[]{50, 52, 54, 56, 58, 60};
+        ArrayAdapter<Integer> adapterUmiMin = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, itemsUmiMin);
+        Integer[] itemsUmiMax = new Integer[]{60, 62, 64, 66, 68, 70};
+        ArrayAdapter<Integer> adapterUmiMax = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, itemsUmiMax);
+        Integer[] itemsLumMin = new Integer[]{500, 520, 540, 560, 580, 600};
+        ArrayAdapter<Integer> adapterLumMin = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, itemsLumMin);
+        Integer[] itemsLumMax = new Integer[]{600, 620, 640, 660, 680, 700};
+        ArrayAdapter<Integer> adapterLumMax = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, itemsLumMax);
+        Spinner mspin = (Spinner) findViewById(R.id.spinner8);
+        mspin.setAdapter(adapterUmiMin);
         mspin = (Spinner) findViewById(R.id.spinner5);
-        mspin.setAdapter(adapter);
-        mspin = (Spinner) findViewById(R.id.spinner6);
-        mspin.setAdapter(adapter);
+        mspin.setAdapter(adapterTempMin);
+        mspin = (Spinner) findViewById(R.id.spinner4);
+        mspin.setAdapter(adapterTempMax);
         mspin = (Spinner) findViewById(R.id.spinner7);
-        mspin.setAdapter(adapter);
-        mspin = (Spinner) findViewById(R.id.spinner8);
-        mspin.setAdapter(adapter);
+        mspin.setAdapter(adapterLumMin);
+        mspin = (Spinner) findViewById(R.id.spinner6);
+        mspin.setAdapter(adapterLumMax);
+        mspin = (Spinner) findViewById(R.id.spinner3);
+        mspin.setAdapter(adapterUmiMax);
     }
 
 
