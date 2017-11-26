3874ed4c8ab692fa7cdba8304ecabdd59b332005
Tuniks <tonikpa@gmail.com>
2017-11-26 18:18:58 -0200

    added http post request with login on main app screen

--- IssuesAuthenticator/app/src/main/java/com/example/user/issuesauthenticator/LabActivity.java
+++ IssuesAuthenticator/app/src/main/java/com/example/user/issuesauthenticator/LabActivity.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-/**
- * Created by Tuniks on 11/15/2017.
- */
 
 public class LabActivity extends AppCompatActivity {
 
--- IssuesAuthenticator/app/src/main/java/com/example/user/issuesauthenticator/MainActivity.java
+++ IssuesAuthenticator/app/src/main/java/com/example/user/issuesauthenticator/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.user.issuesauthenticator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -17,8 +29,35 @@ public class MainActivity extends AppCompatActivity {
     }
     public void preferenceOptions(View view) {
         Intent intent = new Intent(this, LabActivity.class);
-        EditText editText = (EditText) findViewById(R.id.login);
-        String message = editText.getText().toString();
+        EditText loginView = (EditText) findViewById(R.id.login);
+        final String login_text = loginView.getText().toString();
+        EditText senhaView = (EditText) findViewById(R.id.senha);
+        final String senha_text = loginView.getText().toString();
+
+
+        //Fazendo Request HTTP
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://requestb.in/1c4rl0z1";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Debug",response);
+            }}, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", error.getMessage());
+            }}){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("login", login_text);
+                params.put("password", senha_text);
+                return params;
+            }};
+
+        queue.add(stringRequest);
+
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
